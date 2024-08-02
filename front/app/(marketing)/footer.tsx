2ea62b5546c57a22c6eb28ea5ed82015ddcb2a81
Roseweltty <feijaoguida@hotmail.com>
2024-08-02 17:22:29 +0000

    feat: footer component

--- front/app/(marketing)/footer.tsx
+++ front/app/(marketing)/footer.tsx
@@ -1,7 +1,62 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
 const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2" >
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size={'lg'} variant='ghost' className="w-full">
+          <Image
+            src="/flag_br.svg"
+            alt='Brasil'
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Brasil
+        </Button>
+        <Button size={'lg'} variant='ghost' className="w-full">
+          <Image
+            src="/flag_es.svg"
+            alt='Spain'
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Spain
+        </Button>
+        <Button size={'lg'} variant='ghost' className="w-full">
+          <Image
+            src="/flag_us.svg"
+            alt='USA'
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          English
+        </Button>
+        <Button size={'lg'} variant='ghost' className="w-full">
+          <Image
+            src="/flag_hr.svg"
+            alt='Croatian'
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button size={'lg'} variant='ghost' className="w-full">
+          <Image
+            src="/flag_fr.svg"
+            alt='France'
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          France
+        </Button>
+
+      </div>
     </footer>
   )
 }
--- front/app/(marketing)/page.tsx
+++ front/app/(marketing)/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, practice and master new languages with Lingo
         </h1> 
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
         <ClerkLoading>
           <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
         </ClerkLoading>
@@ -48,6 +49,8 @@ export default function Home() {
 
           </SignedIn>
         </ClerkLoaded>
+        </div>
+        
       </div>   
     </div>
   );
