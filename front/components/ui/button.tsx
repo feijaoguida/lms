d730383a9c2c9d642ffbc2d9e1428bab2fde86cf
Roseweltty Guida <feijaoguida@hotmail.com>
2024-08-05 16:25:57 -0300

    feat: Main Layout and other.

--- front/components/ui/button.tsx
+++ front/components/ui/button.tsx
@@ -1,34 +1,41 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 uppercase tracking-wide",
   {
     variants: {
       variant: {
-        default: "bg-white text-black border-slate-200 border-2 border-b-[4px] active:border-b-2 hover:bg-slate-100 text-slate-500",
-        primary: "bg-primaryCobalt-500 text-primary-foreground hover:bg-primaryCobalt-500/90 border-primaryCobalt-400 border-b-4 active:border-b-0",
+        default:
+          "bg-white text-black border-slate-200 border-2 border-b-[4px] active:border-b-2 hover:bg-slate-100 text-slate-500",
+        primary:
+          "bg-primaryCobalt-500 text-primary-foreground hover:bg-primaryCobalt-500/90 border-primaryCobalt-400 border-b-4 active:border-b-0",
         primaryOutline: "bg-white text-primaryCobalt-500 hover:bg-slate-10",
-        secundary: "bg-primaryRose-500 text-primary-foreground hover:bg-primaryRose-400 border-primaryRose-600 border-b-4 active:border-b-0",
-        secundaryOutline: "bg-white text-green-600 hover:bg-slate-10",
-        danger: "bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-0",
+        secundary:
+          "bg-primaryRose-500 text-primary-foreground hover:bg-primaryRose-400 border-primaryRose-600 border-b-4 active:border-b-0",
+        secundaryOutline: "bg-white text-primaryRose-600 hover:bg-slate-10",
+        danger:
+          "bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-0",
         dangerOutline: "bg-white text-rose-600 hover:bg-slate-10",
-        super: "bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0",
+        super:
+          "bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0",
         superOutline: "bg-white text-indigo-600 hover:bg-slate-10",
-        ghost: "bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
-        sidebar: "bg-transparent text-slate-500 border-2 border-transparent hover:bg-slate-100 transition-100",
-        sidebarOutline: "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none",
-
+        ghost:
+          "bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
+        sidebar:
+          "bg-transparent text-slate-500 border-2 border-transparent hover:bg-slate-100 transition-100",
+        sidebarOutline:
+          "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none",
       },
       size: {
         default: "h-11 px-4 py-2",
         sm: "h-9  px-3",
         lg: "h-12 px-8",
         icon: "h-10 w-10",
-        rounded: "rounded-full"
+        rounded: "rounded-full",
       },
     },
     defaultVariants: {
@@ -36,26 +43,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
