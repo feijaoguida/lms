a6cca8b0411345b1cf2fedddf0fa9a71431ce149
Roseweltty Guida <feijaoguida@hotmail.com>
2024-08-11 17:03:09 -0300

    feat: Progress user Course

--- front/app/(main)/courses/page.tsx
+++ front/app/(main)/courses/page.tsx
@@ -1,13 +1,22 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgres } from "@/db/queries";
 import { List } from "./list";
 
 const CoursesPage = async () => {
-  const dataCourses = await getCourses();
+  const dataCoursesPromise = getCourses();
+  const userProgressPromise = getUserProgres();
+
+  const [dataCourses, userProgress] = await Promise.all([
+    dataCoursesPromise,
+    userProgressPromise,
+  ]);
 
   return (
     <div className="h-full max-w-[912px] mx-auto px-3">
       <h1 className="text-2xl font-bold text-neutral-700">Courses</h1>
-      <List courses={dataCourses} activeCourseId={dataCourses[0].id} />
+      <List
+        courses={dataCourses}
+        activeCourseId={userProgress?.activeCourseId}
+      />
     </div>
   );
 };
