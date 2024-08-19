4597cdfd6f2ccc0c49dcdb16aa37c6ee00411eed
Roseweltty Guida <feijaoguida@hotmail.com>
2024-08-19 10:25:01 -0300

    feat: cards challenges,

--- front/app/(main)/lesson/card.tsx
+++ front/app/(main)/lesson/card.tsx
@@ -1,6 +1,8 @@
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useCallback } from "react";
+import { useAudio, useKey } from "react-use";
 
 type Props = {
   id: number;
@@ -27,10 +29,23 @@ export const Card = ({
   disabled,
   type,
 }: Props) => {
+  const [audio, _, controls] = useAudio({
+    src: audioSrc || "",
+  });
+
+  const handleClick = useCallback(() => {
+    if (disabled) return;
+
+    controls.play();
+    onClick();
+  }, [disabled, onClick, controls]);
+
+  useKey(shortcut, handleClick, {}, [handleClick]);
+
   return (
     <>
       <div
-        onClick={() => {}}
+        onClick={handleClick}
         className={cn(
           "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
           selected &&
@@ -45,12 +60,43 @@ export const Card = ({
           type === "ASSIST" && "lg:p-3 w-full"
         )}
       >
+        {audio}
         {imageSrc && (
           <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
             <Image src={imageSrc} alt={text} fill />
           </div>
         )}
-        {text}
+        <div
+          className={cn(
+            "flex items-center justify-between",
+            type === "ASSIST" && "flex-row-reverse"
+          )}
+        >
+          {" "}
+          {type === "ASSIST" && <div />}
+          <p
+            className={cn(
+              "text-neutral-600 text-sm lg:text-base",
+              selected && "text-primaryCobalt-500",
+              selected && status === "correct" && "text-green-500",
+              selected && status === "wrong" && "text-rose-500"
+            )}
+          >
+            {text}
+          </p>
+          <div
+            className={cn(
+              "lg:w-[30px] lg:h-[30px] w-[20px] h-[20px] border-2 flex items-center justify-center rounded-lg text-neutral-400 lg:text-[15px] text-xs font-semibold",
+              selected && "text-primaryCobalt-500 border-primaryCobalt-300",
+              selected &&
+                status === "correct" &&
+                "text-green-500 border-green-300",
+              selected && status === "wrong" && "text-rose-500 border-rose-300"
+            )}
+          >
+            {shortcut}
+          </div>
+        </div>
       </div>
     </>
   );
